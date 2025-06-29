174f2e14a9f8c155dd0e268eaf970a167c0bba96
syntax <tfeholland@gmail.com>
2025-06-29 11:27:59 +0100

    add changes to test fiel

--- TestFile.java
+++ TestFile.java
@@ -1,22 +1,68 @@
 package org.pdgdiff.testclasses;
 
-public class TestFileBefore {
+public class TestFileAfter {
 
-    private int onefield;
-
-    public String anotherfield;
+    private int thefield;
 
     public static void main(String[] args) {
-        TestFileBefore test = new TestFileBefore();
-        int result = test.addNumbers(5, 10);
+        TestFileAfter test = new TestFileAfter();
+        int number1 = 5;
+        int number2 = 4;
+        int result = test.addNumbers(number1, number2);
         System.out.println("Result: " + result);
-        int res = test.minus(10, 5);
-        System.out.println("Result: " + res);
+        int product = test.multiplyNumbers(number1, number2);
+        System.out.println("Product: " + product);
 
-        int complexRes = test.detailedComputation(5, 10);
-        System.out.println("Detailed Computation Result: " + complexRes);
-        int t = test.identical(5, 10);
-        System.out.println("identical Result: " + t);
+        int complexResult = test.complexCalculation(number1, number2);
+        int identical_out = test.identical(3, 10);
+        System.out.println("Complex Calculation Result: " + complexResult);
+        System.out.println("identical Result: " + identical_out);
+    }
+
+    public int addNumbers(int number, int number2) {
+        int sum = number + number2;
+        return sum;
+    }
+
+    public int multiplyNumbers(int number, int number2) {
+        int product = number * number2;
+        return product;
+    }
+
+    // added these more complex classes with more intense control flow and
+    // non-matching names to try and catch edge cases
+    public int complexCalculation(int num1, int num2) {
+        int result = 0;
+
+        // Conditional statements
+        if (num1 > num2) {
+            result = num1 - num2;
+        } else if (num1 < num2) {
+            result = num1 + num2;
+        } else {
+            result = num1 * num2;
+        }
+
+        // Loop that performs additional operations
+        for (int i = 0; i < 3; i++) {
+            result += i;
+            if (result % 2 == 0) {
+                result /= 2;
+            } else {
+                result *= 3;
+            }
+        }
+
+        // Nested conditional inside a loop
+        for (int i = 0; i < 5; i++) {
+            if (i % 2 == 0) {
+                result += i;
+            } else {
+                result -= i;
+            }
+        }
+
+        return result;
     }
 
     public int identical(int num1, int num2) {
@@ -32,51 +78,4 @@ public class TestFileBefore {
         }
         return result;
     }
-
-    public int addNumbers(int a, int b) {
-        int toadd1 = a;
-        int toadd2 = b;
-        int sum = toadd1 + toadd2;
-        return sum;
-    }
-
-    public int minus(int a, int b) {
-        int sum = a - b;
-        return sum;
-    }
-
-    // added these more complex classes with more intense control flow and
-    // non-matching names to try and catch edge cases
-    public int detailedComputation(int num1, int num2) {
-        int result = 0;
-
-        // Conditional statements
-        if (num1 > num2) {
-            result = num1 + num2;
-        } else if (num1 < num2) {
-            result = num1 - num2;
-        } else {
-            result = num1 * num2;
-        }
-
-        // Loop that performs additional operations
-        for (int i = 0; i < 4; i++) {
-            result -= i;
-            if (result % 3 == 0) {
-                result /= 3;
-            } else {
-                result += i * 2;
-            }
-        }
-
-        // Nested conditional inside a loop
-        for (int i = 0; i < 6; i++) {
-            if (i % 2 == 1) {
-                result *= i;
-            }
-        }
-
-        return result;
-    }
-
 }
